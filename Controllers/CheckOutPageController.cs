d116be50ab081ae45c12d9c2184bc8bb754ad7be
Maryna <textconstruction@protonmail.com>
2021-11-05 22:22:02 +0100

    Service Checkout Page

---
+++ Controllers/CheckOutPageController.cs
@@ -0,0 +1,38 @@
+﻿using FreakyFashionPoweredByUmbraco.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace FreakyFashionPoweredByUmbraco.Controllers
+{
+    public class CheckOutPageController : RenderController
+    {
+        private readonly IVariationContextAccessor _variationContextAccessor;
+        private readonly ServiceContext _serviceContext;
+        
+        public CheckOutPageController(ILogger<RenderController> logger, 
+                                      ICompositeViewEngine compositeViewEngine, 
+                                      IUmbracoContextAccessor umbracoContextAccessor,
+                                      IVariationContextAccessor variationContextAccessor,
+                                      ServiceContext serviceContext) 
+            : base(logger, compositeViewEngine, umbracoContextAccessor)
+        {
+            _variationContextAccessor = variationContextAccessor;
+            _serviceContext = serviceContext;
+        }
+
+        public override IActionResult Index()
+        {
+            var viewModel = new CheckOutPageViewModel(CurrentPage, new PublishedValueFallback(_serviceContext, _variationContextAccessor));
+            return CurrentTemplate(viewModel);
+        }
+    }
+}
---
+++ Models/ViewModels/CheckOutPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace FreakyFashionPoweredByUmbraco.Models.ViewModels
+{
+    public class CheckOutPageViewModel : CheckOutPage
+    {
+        public CheckOutPageViewModel(IPublishedContent content, IPublishedValueFallback publishedValueFallback) 
+            : base(content, publishedValueFallback)
+        {
+        }
+    }
+}
